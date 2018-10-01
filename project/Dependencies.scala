e8362bc2df437db80333dfa31137dcd4121a316d
Mukund Ananthu <mukund.code@gmail.com>
2018-10-01 19:55:36 +0530

    Bump cats-core to 1.4.0(Closes #39)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ import sbt._
 
 object Dependencies {
   object V {
-    val catsCore = "1.1.0"
+    val catsCore = "1.4.0"
     val catsEffect = "1.0.0"
     val circe = "0.10.0"
     val specs2 = "4.2.0"

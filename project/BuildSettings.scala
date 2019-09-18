b5fdecf52e05a5efdf24ebdf603b8ea9bd850c3c
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2019-09-18 16:10:07 +0200

    Update referrer list in 0.3.0 (closes #75)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,19 +1,18 @@
 /**
- * Copyright 2012-2013 Snowplow Analytics Ltd
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+  * Copyright 2012-2013 Snowplow Analytics Ltd
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 import sbt._
 import Keys._
 
@@ -21,13 +20,13 @@ object BuildSettings {
 
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
-    organization  := "com.snowplowanalytics",
-    version       := "0.3.0",
-    description   := "Library for extracting marketing attribution data from referer URLs",
-    scalaVersion  := "2.9.1",
+    organization := "com.snowplowanalytics",
+    version := "0.3.1",
+    description := "Library for extracting marketing attribution data from referer URLs",
+    scalaVersion := "2.11.1",
     crossScalaVersions := Seq("2.9.1", "2.10.4", "2.11.1"),
     scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
-    resolvers     ++= Dependencies.resolutionRepos
+    resolvers ++= Dependencies.resolutionRepos
   )
 
   // Publish settings
@@ -35,12 +34,13 @@ object BuildSettings {
   lazy val publishSettings = Seq[Setting[_]](
     // Enables publishing to maven repo
     publishMavenStyle := true,
-
     publishTo <<= version { version =>
       val basePath = "target/repo/%s".format {
         if (version.trim.endsWith("SNAPSHOT")) "snapshots/" else "releases/"
       }
-      Some(Resolver.file("Local Maven repository", file(basePath)) transactional())
+      Some(
+        Resolver.file("Local Maven repository", file(basePath)) transactional ()
+      )
     }
   )
 
--- src/main/scala/com/snowplowanalytics/refererparser/scala/Parser.scala
+++ src/main/scala/com/snowplowanalytics/refererparser/scala/Parser.scala
@@ -1,77 +1,117 @@
 /**
- * Copyright 2012-2013 Snowplow Analytics Ltd
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+  * Copyright 2012-2013 Snowplow Analytics Ltd
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 package com.snowplowanalytics.refererparser.scala
 
 // Java
+import java.io.ByteArrayInputStream
 import java.net.{URI, URISyntaxException}
+
+import scala.util.Failure
 
 // RefererParser Java impl
 import com.snowplowanalytics.refererparser.{Parser => JParser}
 import com.snowplowanalytics.refererparser.{Medium => JMedium}
 
 // Scala
-import scala.collection.JavaConversions._ 
+import scala.collection.JavaConversions._
+import scala.util.Try
+import com.typesafe.scalalogging.Logger
 
 /**
- * Enumeration for supported mediums.
- *
- * Replacement for Java version's Enum.
- */
+  * Enumeration for supported mediums.
+  *
+  * Replacement for Java version's Enum.
+  */
 object Medium extends Enumeration {
   type Medium = Value
 
-  val Unknown  = Value("unknown")
-  val Search   = Value("search")
+  val Unknown = Value("unknown")
+  val Search = Value("search")
   val Internal = Value("internal")
-  val Social   = Value("social")
-  val Email    = Value("email")
+  val Social = Value("social")
+  val Email = Value("email")
+  val Paid = Value("paid")
 
   /**
-   * Converts from our Java Medium Enum
-   * to our Scala Enumeration values above.
-   */
+    * Converts from our Java Medium Enum
+    * to our Scala Enumeration values above.
+    */
   def fromJava(medium: JMedium) =
     Medium.withName(medium.toString())
 }
 
 /**
- * Immutable case class to hold a referer.
- *
- * Replacement for Java version's POJO.
- */
+  * Immutable case class to hold a referer.
+  *
+  * Replacement for Java version's POJO.
+  */
 case class Referer(
-  medium: Medium.Medium,
-  source: Option[String],
-  term:   Option[String]
+    medium: Medium.Medium,
+    source: Option[String],
+    term: Option[String]
 )
 
 /**
- * Parser object - contains one-time initialization
- * of the YAML database of referers, and parse()
- * methods to generate a Referer object from a
- * referer URL.
- *
- * In Java this had to be instantiated as a class.
- */
+  * Parser object - contains one-time initialization
+  * of the YAML database of referers, and parse()
+  * methods to generate a Referer object from a
+  * referer URL.
+  *
+  * In Java this had to be instantiated as a class.
+  */
 object Parser {
 
   type MaybeReferer = Option[Referer]
 
-  private lazy val jp = new JParser()
+  val logger = Logger[Parser.type]
+
+  private lazy val jp: JParser = createParser
+
+  val createParser: JParser = {
+    import org.apache.http.client.fluent.Request
+    import org.apache.http.util.EntityUtils
+
+    val LatestReferrerUrl =
+      "https://s3-eu-west-1.amazonaws.com/snowplow-hosted-assets/third-party/referer-parser/referers-latest.yml"
+
+    lazy val latestParser =
+      for {
+        _ <- Try(logger.info(s"Trying to load referrers from $LatestReferrerUrl"))
+        response <- Try(Request.Get(LatestReferrerUrl).execute().returnResponse())
+        statusCode <- Try(response.getStatusLine.getStatusCode)
+        if statusCode == 200
+        inStream <- Try(new ByteArrayInputStream(EntityUtils.toByteArray(response.getEntity)))
+        parser <- Try(new JParser(inStream))
+      } yield parser
+
+    latestParser.transform({ p =>
+      logger.info("Latest referrers loaded successfully")
+      Try(p)
+    }, { e =>
+      logger.warn(s"Errors: ${e.getMessage}")
+      Failure(e)
+    })
+
+    lazy val hardcodedParser = {
+      logger.warn("Falling back to hardcoded referrers")
+      new JParser()
+    }
+
+    latestParser.getOrElse(hardcodedParser)
+  }
 
   private def getHostSafely(uri: URI): String = {
     if (uri == null) {
@@ -82,46 +122,58 @@ object Parser {
   }
 
   /**
-   * Parses a `refererUri` UR and a `pageUri`
-   * URI to return either Some Referer, or None.
-   */
+    * Parses a `refererUri` UR and a `pageUri`
+    * URI to return either Some Referer, or None.
+    */
   def parse(refererUri: URI, pageUri: URI): MaybeReferer =
     parse(refererUri, getHostSafely(pageUri), Nil);
 
   /**
-   * Parses a `refererUri` UR and a `pageUri`
-   * URI to return either Some Referer, or None.
-   */
-  def parse(refererUri: URI, pageUri: URI, internalDomains: List[String]): MaybeReferer =
+    * Parses a `refererUri` UR and a `pageUri`
+    * URI to return either Some Referer, or None.
+    */
+  def parse(
+      refererUri: URI,
+      pageUri: URI,
+      internalDomains: List[String]
+  ): MaybeReferer =
     parse(refererUri, getHostSafely(pageUri), internalDomains);
 
   /**
-   * Parses a `refererUri` String and a `pageUri`
-   * URI to return either Some Referer, or None.
-   */
+    * Parses a `refererUri` String and a `pageUri`
+    * URI to return either Some Referer, or None.
+    */
   def parse(refererUri: String, pageUri: URI): MaybeReferer =
     parse(refererUri, getHostSafely(pageUri), Nil);
 
   /**
-   * Parses a `refererUri` String and a `pageUri`
-   * URI to return either Some Referer, or None.
-   */
-  def parse(refererUri: String, pageUri: URI, internalDomains: List[String]): MaybeReferer =
+    * Parses a `refererUri` String and a `pageUri`
+    * URI to return either Some Referer, or None.
+    */
+  def parse(
+      refererUri: String,
+      pageUri: URI,
+      internalDomains: List[String]
+  ): MaybeReferer =
     parse(refererUri, getHostSafely(pageUri), internalDomains);
 
   /**
-   * Parses a `refererUri` String and a `pageUri`
-   * URI to return either some Referer, or None.
-   */
+    * Parses a `refererUri` String and a `pageUri`
+    * URI to return either some Referer, or None.
+    */
   def parse(refererUri: String, pageHost: String): MaybeReferer = {
     parse(refererUri, pageHost, Nil)
   }
 
   /**
-   * Parses a `refererUri` String and a `pageUri`
-   * URI to return either some Referer, or None.
-   */
-  def parse(refererUri: String, pageHost: String, internalDomains: List[String]): MaybeReferer = {
+    * Parses a `refererUri` String and a `pageUri`
+    * URI to return either some Referer, or None.
+    */
+  def parse(
+      refererUri: String,
+      pageHost: String,
+      internalDomains: List[String]
+  ): MaybeReferer = {
 
     if (refererUri == null || refererUri == "") {
       None
@@ -135,24 +187,32 @@ object Parser {
   }
 
   /**
-   * Parses a `refererUri` URI to return
-   * either Some Referer, or None.
-   */
+    * Parses a `refererUri` URI to return
+    * either Some Referer, or None.
+    */
   def parse(refererUri: URI, pageHost: String): MaybeReferer = {
     parse(refererUri, pageHost, Nil)
-}
-
+  }
 
   /**
-   * Parses a `refererUri` URI to return
-   * either Some Referer, or None.
-   */
-  def parse(refererUri: URI, pageHost: String, internalDomains: List[String]): MaybeReferer = {
-    
+    * Parses a `refererUri` URI to return
+    * either Some Referer, or None.
+    */
+  def parse(
+      refererUri: URI,
+      pageHost: String,
+      internalDomains: List[String]
+  ): MaybeReferer = {
+
     try {
       val jrefr = Option(jp.parse(refererUri, pageHost, internalDomains))
-      jrefr.map(jr =>
-        Referer(Medium.fromJava(jr.medium), Option(jr.source), Option(jr.term))
+      jrefr.map(
+        jr =>
+          Referer(
+            Medium.fromJava(jr.medium),
+            Option(jr.source),
+            Option(jr.term)
+          )
       )
     } catch {
       case use: URISyntaxException => None

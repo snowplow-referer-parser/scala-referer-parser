3e958be78c1610045e5d459b42a9bfdd72eb9a4d
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2019-10-01 10:20:53 +0200

    Update Scala to 2.11 in 0.3.x branch (closes #83)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -24,7 +24,7 @@ object BuildSettings {
     organization  := "com.snowplowanalytics",
     version       := "0.3.0",
     description   := "Library for extracting marketing attribution data from referer URLs",
-    scalaVersion  := "2.9.1",
+    scalaVersion  := "2.11.1",
     crossScalaVersions := Seq("2.9.1", "2.10.4", "2.11.1"),
     scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
     resolvers     ++= Dependencies.resolutionRepos

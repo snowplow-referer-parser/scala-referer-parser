da48f27267135daf039b46761cefc30224e00b45
Łukasz Indykiewicz <lukasz.indykiewicz@gmail.com>
2020-09-03 21:26:46 +0200

    Fix cross-build (close #115)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,20 +17,21 @@ import sbt._
 
 object Dependencies {
   object V {
-    val catsCore   = "2.1.1"
-    val catsEffect = "2.1.4"
-    val circe      = "0.13.0"
-    val specs2     = "4.10.3"
+    val catsCore         = "2.1.1"
+    val catsEffect       = "2.1.4"
+    val circe            = "0.13.0"
+    val collectionCompat = "2.1.6"
+    val specs2           = "4.10.3"
   }
 
   object Libraries {
-    val catsCore     = "org.typelevel" %% "cats-core"     % V.catsCore
-    val catsEffect   = "org.typelevel" %% "cats-effect"   % V.catsEffect
-    val circeCore    = "io.circe"      %% "circe-core"    % V.circe
-    val circeGeneric = "io.circe"      %% "circe-generic" % V.circe
-    val circeParser  = "io.circe"      %% "circe-parser"  % V.circe
-
-    val specs2Core       = "org.specs2" %% "specs2-core"       % V.specs2 % Test
-    val specs2Scalacheck = "org.specs2" %% "specs2-scalacheck" % V.specs2 % Test
+    val catsCore         = "org.typelevel"          %% "cats-core"               % V.catsCore
+    val catsEffect       = "org.typelevel"          %% "cats-effect"             % V.catsEffect
+    val circeCore        = "io.circe"               %% "circe-core"              % V.circe
+    val circeGeneric     = "io.circe"               %% "circe-generic"           % V.circe
+    val circeParser      = "io.circe"               %% "circe-parser"            % V.circe
+    val collectionCompat = "org.scala-lang.modules" %% "scala-collection-compat" % V.collectionCompat
+    val specs2Core       = "org.specs2"             %% "specs2-core"             % V.specs2 % Test
+    val specs2Scalacheck = "org.specs2"             %% "specs2-scalacheck"       % V.specs2 % Test
   }
 }

209965872e75e783fb13c8f74e1f53d711ff2244
gipeshka <gipeshka@gmail.com>
2021-04-27 19:12:48 +0200

    update core version to latest

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ import sbt._
 
 object Dependencies {
   object V {
-    val catsCore         = "2.2.0"
+    val catsCore         = "2.6.0"
     val catsEffect       = "3.1.0"
     val circe            = "0.13.0"
     val collectionCompat = "2.1.6"

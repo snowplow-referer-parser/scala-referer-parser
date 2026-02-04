fbddd611bc664a12b3a49af7c36c12b580fc84df
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2026-02-04 13:51:06 +0100

    Upgrade Java to 21 (#130)

--- build.sbt
+++ build.sbt
@@ -1,26 +1,22 @@
 /**
-  * Copyright 2012-2022 Snowplow Analytics Ltd
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2012-present Snowplow Analytics Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 val root = (project in file("."))
+  .settings(BuildSettings.buildSettings)
+  .settings(BuildSettings.publishSettings)
+  .settings(BuildSettings.docSettings)
+  .enablePlugins(SiteScaladocPlugin, PreprocessPlugin)
   .settings(
-    name := "scala-referer-parser",
-    organization := "com.snowplowanalytics",
-    description := "Library for extracting marketing attribution data from referer URLs",
-    scalaVersion := "2.13.8",
-    crossScalaVersions := Seq("2.12.15", "2.13.8"),
-    javacOptions := BuildSettings.javaCompilerOptions,
     libraryDependencies ++= Seq(
       Dependencies.Libraries.catsCore,
       Dependencies.Libraries.catsEffect,
@@ -32,6 +28,3 @@ val root = (project in file("."))
       Dependencies.Libraries.specs2Scalacheck
     )
   )
-  .settings(BuildSettings.publishSettings)
-  .settings(BuildSettings.docSettings)
-  .enablePlugins(SiteScaladocPlugin, PreprocessPlugin)
--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,21 +1,22 @@
 /**
-  * Copyright 2012-2022 Snowplow Analytics Ltd
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *      http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright 2012-present Snowplow Analytics Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 // Sbt
 import sbt.Keys._
 import sbt._
+
+// scalafmt
+import org.scalafmt.sbt.ScalafmtPlugin.autoImport._
 
 // dynver plugin
 import sbtdynver.DynVerPlugin.autoImport._
@@ -26,11 +27,39 @@ import com.typesafe.sbt.site.SiteScaladocPlugin.autoImport.SiteScaladoc
 
 object BuildSettings {
 
+  lazy val scala212 = "2.12.20"
+  lazy val scala213 = "2.13.16"
+
+  lazy val buildSettings = Seq(
+    organization := "com.snowplowanalytics",
+    name := "scala-referer-parser",
+    description := "Library for extracting marketing attribution data from referer URLs",
+    scalaVersion := scala213,
+    crossScalaVersions := List(scala212, scala213),
+    scalafmtConfig := file(".scalafmt.conf"),
+    scalafmtOnCompile := false,
+    scalacOptions ++= scalacOptionsVersion(scalaVersion.value),
+    resolvers ++= Seq(
+      "Snowplow Analytics Maven repo".at("http://maven.snplow.com/releases/").withAllowInsecureProtocol(true)
+    )
+  )
+
+  def scalacOptionsVersion(scalaVersion: String): Seq[String] = {
+    // Scala 2.12 needs -Ywarn-macros:after to match the default compiler behaviour of scala 2.13.
+    val versionSpecificOptions = CrossVersion.partialVersion(scalaVersion) match {
+      case Some((2, scalaMajor)) if scalaMajor == 12 => Seq("-Ywarn-macros:after")
+      case _                                         => Nil
+    }
+    Seq(
+      "-Wconf:origin=scala.collection.compat.*:s"
+    ) ++ versionSpecificOptions
+  }
+
   lazy val publishSettings = Seq[Setting[_]](
     publishArtifact := true,
     Test / publishArtifact := false,
     pomIncludeRepository := { _ => false },
-    homepage := Some(url("http://snowplowanalytics.com")),
+    homepage := Some(url("https://snowplow.io")),
     licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html")),
     ThisBuild / dynverVTagPrefix := false, // Otherwise git tags required to have v-prefix
     developers := List(
@@ -38,11 +67,10 @@ object BuildSettings {
         "Snowplow Analytics Ltd",
         "Snowplow Analytics Ltd",
         "support@snowplowanalytics.com",
-        url("https://snowplowanalytics.com")
+        url("https://snowplow.io")
       )
     )
   )
-  lazy val javaCompilerOptions = Seq("-source", "11", "-target", "11")
 
   lazy val docSettings = Seq(
     SiteScaladoc / siteSubdirName := s"${version.value}"
